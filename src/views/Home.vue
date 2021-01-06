b5788201bd44339dbfb07182d3ee20d4db582608
kyawzin <kyawzinmcci00158313@gmail.com>
2021-01-06 19:17:26 +0630

    add win status and track remaining pairs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,7 +16,7 @@
 
 <script>
 // @ is an alias to /src
-import {ref, watch} from "vue"
+import {computed, ref, watch} from "vue"
 import Card from "@/components/Card";
 
 export default {
@@ -27,16 +27,28 @@ export default {
   setup() {
     const cardList = ref([]);
     const userSelection = ref([]);
-    const status = ref('');
+    const status = computed(()=>{
+      if (remainingPairs.value === 0){
+        return "Player Win!"
+      }else {
+        return `Remaining Pairs ${remainingPairs.value}`
+      }
+    });
 
     for (let i = 0; i < 16; i++) {
       cardList.value.push({
-        value: i,
+        value: i === 8 ? 0 : i > 7 ? i - 8 : i,
         visible: false,
         position: i,
         matched: false
       });
     }
+    console.log(cardList.value)
+
+    const remainingPairs = computed(() => {
+      const remainingCards = cardList.value.filter(card => card.matched === false).length;
+      return remainingCards / 2;
+    })
 
     const flipCard = (payload) => {
       cardList.value[payload.position].visible = true;
@@ -72,6 +84,7 @@ export default {
       cardList,
       userSelection,
       status,
+      remainingPairs,
       flipCard
     };
   }
