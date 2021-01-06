5c2d2b7daa47fd320a8e2d0bf5e85e9d2613d025
kyawzin <kyawzinmcci00158313@gmail.com>
2021-01-06 18:21:55 +0630

    track user selection and validate choices

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,20 +2,23 @@
   <h1>Peek a Vue</h1>
   <section class="game-board">
     <Card
-        v-for="(card, index) in cardList"
-        :key="`card-${index}`"
-        :value="card.value"
-        :visible="card.visible"
-        :position="card.position"
-        @select-card="flipCard"
+      v-for="(card, index) in cardList"
+      :key="`card-${index}`"
+      :value="card.value"
+      :visible="card.visible"
+      :position="card.position"
+      :matched="card.matched"
+      @select-card="flipCard"
     />
   </section>
+  <h2>{{ status }}</h2>
 </template>
 
 <script>
 // @ is an alias to /src
-import {ref} from "vue"
+import {ref, watch} from "vue"
 import Card from "@/components/Card";
+
 export default {
   name: "Home",
   components: {
@@ -23,21 +26,52 @@ export default {
   },
   setup() {
     const cardList = ref([]);
+    const userSelection = ref([]);
+    const status = ref('');
 
     for (let i = 0; i < 16; i++) {
       cardList.value.push({
         value: i,
         visible: false,
-        position: i
+        position: i,
+        matched: false
       });
     }
 
     const flipCard = (payload) => {
       cardList.value[payload.position].visible = true;
+
+      if (userSelection.value[0]) {
+        userSelection.value[1] = payload;
+      } else {
+        userSelection.value[0] = payload;
+      }
     }
+
+    watch(userSelection, (currentValue) => {
+      if (currentValue.length === 2){
+        // console.log(currentValue[0]);
+        const cardOne = currentValue[0];
+        const cardTwo = currentValue[1];
+
+        if (cardOne.faceValue === cardTwo.faceValue){
+          status.value = 'Matched!';
+          cardList.value[cardOne.position].matched = true;
+          cardList.value[cardTwo.position].matched = true;
+        }else {
+          status.value = 'Mismatched!';
+          cardList.value[cardOne.position].visible = false;
+          cardList.value[cardTwo.position].visible = false;
+        }
+
+        userSelection.value.length = 0;
+      }
+    }, { deep: true})
 
     return {
       cardList,
+      userSelection,
+      status,
       flipCard
     };
   }
